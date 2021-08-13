9ed04f6a76198a9775b181ef1234eba647d634a5
sadamalsharabi <44142745+sadamshr3be@users.noreply.github.com>
2021-08-13 06:47:21 +0200

    Add files via upload

---
+++ AhmiiVirus.sh
@@ -0,0 +1,160 @@
+# A tool created by Ahmii Ch
+# Be a unique person not copy cat
+# Colours
+#-----------------
+Red =  '\033[1;31m'
+Rset=  '\033[0m'
+Green= '\033[1;32m'
+Yellow='\033[1;33m'
+Blue=  '\033[1;34m'
+Cyan=  '\033[1;36m'
+Pink=  '\033[1;35m'
+#-----------------
+# scripting starts
+clear
+echo '
+                
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X
+X  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   Virus  v1.1      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                       Baba Gangster
+'
+echo " " 
+
+echo -e "$red                      ▶ Coded by$Ahmi Ch$red ◀$rset"
+echo -e "$red                 ⫸$ylo    Baba alsharabi$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                   [Infect them with fun]$rset"
+echo " "
+echo " "
+echo -e "$grn              ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [1️⃣] Infect now"
+echo -e "$red                        ➡$cyan [2️⃣] Save now"
+echo -e "$red                        ➡️$cyan [3️⃣] Update script"
+echo -e "$red                        ➡$cyan [4️⃣] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -e "$cyan                   [[[$ylo Select any option$cyan]]]$rset"
+echo " "
+read n
+case "$n" in
+1)echo " "
+echo " "
+echo " "
+echo -e "$red               You are going to$grn Infect$red Now$rset"
+echo " "
+echo -e "$grn                  Press$ylo ENTER$red to continue$rset"
+echo " "
+read hulke
+cd $HOME/AhmiiVirus
+echo " "
+echo -e "$ylo     To terminate the process click$red 't'$ylo or to continue click on$grn ENTER$rset" 
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                    Are you sure? Press$grn ENTER$red to exit$rset"
+read hulkee
+cd $HOME/AhmiiVirus/
+bash AhmiiVirus.sh
+else
+bash sysups.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan            click$grn ENTER$cyan to go back into infect  menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/AhmiiVirus/
+bash AhmiiVirus.sh ;;
+2)echo " "
+echo " "
+echo " "
+echo -e "$red                You are going to$red Save victim$rset "
+echo " "
+echo -e "$grn                  Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nitro
+cd $HOME/AhmiiVirus/
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                   Are you sure? Press$grn ENTER$red to exit$rset"
+read nitroo
+cd $HOME/AhmiiVirus
+bash AhmiiVirus.sh
+else
+bash Saves.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan             click$grn ENTER$cyan to go back into infect  menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/AhmiiVirus
+bash AhmiiVirus.sh ;;
+3)echo " "
+echo " "
+echo " "
+echo -e "$red             You are going to$grn Update the$red Script$rset "
+echo " "
+echo -e "$grn                 Press$ylo ENTER$grn to continue$rset"
+echo " "
+read mon
+cd $HOME/AhmiiVirus/
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                 Are you sure? Press$grn ENTER$red to exit$rset"
+read monn
+cd $HOME/AhmiiVirus
+bash AhmiiVirus.sh
+else
+bash Updates.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan        click$grn ENTER$cyan to go back into infect  menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/AhmiiVirus
+bash AhmiiVirus.sh ;;
+4)
+sleep 2.0
+clear
+echo " "
+echo -e "$grn
+     ____  _  _  ____    ____  _  _  ___
+    (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+     ) _ < \  /  )__)    ) _ < \  /  )__)
+    (____/ (__) (____)  (____/ (__) (____)...$cyan have a awesome day$rset"
+
+echo " "
+echo " "
+esac
